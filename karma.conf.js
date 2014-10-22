f07180adf7f3165e5e2dd345b8a907732f17359b
Maria Lynch <ml26g11@soton.ac.uk>
2014-10-22 15:34:00 +0100

    Basic set up to use UI Bootstrap

--- karma.conf.js
+++ karma.conf.js
@@ -7,8 +7,11 @@ module.exports = function(config){
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/bower_components/angular-sanitize/angular-sanitize.js',
+	  'app/bower_components/angular-bootstrap/ui-bootstrap.js',
+	  'app/bower_components/bootstrap/dist/js/bootstrap.js',
       'app/components/**/*.js',
-      'app/view*/**/*.js'
+      'app/authoring/**/*.js'
     ],
 
     autoWatch : true,
